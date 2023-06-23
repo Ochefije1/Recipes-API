a52943cd3ee3f01a5dffa18ef0c42ef5832ed91b
Adikwu Samuel <Adikwu.S@bullseye.local>
2023-06-23 17:36:11 +0100

    local recipe

--- Recipes_api/recipes_api/api/models.py
+++ Recipes_api/recipes_api/api/models.py
@@ -1,17 +1,28 @@
+from rest_framework import serializers
 from django.db import models
-# from django.core.validators import FileExtensionValidator
 
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
-    ingredients = models.TextField()
-    instructions = models.TextField()
-    # time = models.IntegerField()
 
     def __str__(self):
         return self.title
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
@@ -22,10 +33,31 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-    recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Ingredient.objects.all())
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Tag.objects.all())
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'description', 'ingredients', 'tags']
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name', 'description', 'quantity']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
